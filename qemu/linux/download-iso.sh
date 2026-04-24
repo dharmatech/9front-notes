2a7453bf46255db196af0269714f1432a911ffea
dharmatech <wayo.cavazos@gmail.com>
2026-04-24 06:49:26 -0700

    Add scripts for downloading and installing 9front ISO

---
+++ qemu/linux/download-iso.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://9front.org/iso/9front-11554.amd64.iso.gz
+
+gzip -d 9front-11554.amd64.iso.gz
+
---
+++ qemu/linux/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ISO='9front-11551.amd64.iso'
+
+ISO='9front-11554.amd64.iso'
+
+qemu-system-x86_64 -m 1024 \
+    -net nic,model=virtio,macaddr=00:20:91:37:33:77 -net user \
+    -device virtio-scsi-pci,id=scsi \
+    -drive if=none,id=vd0,file=9front.qcow2.img \
+    -device scsi-hd,drive=vd0 \
+    -drive if=none,id=vd1,file=$ISO,format=raw \
+    -device scsi-cd,drive=vd1,bootindex=0
